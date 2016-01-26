bab0893caf0cdf40ae0bab09f99293cb7a062925
ubuntu <ubuntu@ppathak.ezdi-domain.lan>
2016-01-26 13:13:51 +0530

    DataEntry was done

--- SalesTool/src/SalesTool/SalesData.java
+++ SalesTool/src/SalesTool/SalesData.java
@@ -2,9 +2,14 @@ package SalesTool;
 
 public class SalesData
 {
-	int data[] = {0,4,42};
+	int data[] = {0,4,42},i;
 	public void display()
 		{
-			System.out.println("TODO : Implement data display");
+			System.out.println("Data is as given below . . .");
+			
+			for (i=0;i<data.length;i++)
+			{
+				System.out.println("Next Data is : " + data[i]);
+			}
 		}
 }
